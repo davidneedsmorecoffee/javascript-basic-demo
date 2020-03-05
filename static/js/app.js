8848507603a05c9fd94c96bf3552d7e16ae05c9c
David <davidlin.ubc@gmail.com>
2020-03-05 08:31:04 -0600

    repo clean up

--- static/js/app.js
+++ static/js/app.js
@@ -1,12 +1,7 @@
 // from data.js
 var tableData = data;
 
-// INSTRUCTIONS
-// Level 1: Automatic Table and Date Search (Required)
-// Create a basic HTML web page or use the index.html file provided 
-// (we recommend building your own custom page!).
-// Using the UFO dataset provided in the form of an array of JavaScript objects, 
-// write code that appends a table to your web page and then adds new rows of data for each UFO sighting.
+
 
 // Make sure you have a column for date/time, city, state, country, shape, and comment at the very least.
 var columns = ["datetime", "city", "state", "country", "shape", "durationMinutes", "comments"]
